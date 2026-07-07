05e146b3528b9532b598f46318b348c048b3936c
anthonyochoa99 <aeochoa99@outlook.com>
2026-07-07 03:48:11 -0700

    Add a function that returns the players choice

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const choices = ["rock", "paper", "scissors"];
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
+
+function getPlayerChoice() {
+    choice = prompt("Enter 'rock', 'paper', or scissors'.").toLowerCase().replace(/\s/g, '');
+
+    if (choice !== choices[0] && choice !== choices[1] && choice !== choices[2]) {
+        return "Please enter a valid choice.";
+    }
+
+    return choice;
+}
